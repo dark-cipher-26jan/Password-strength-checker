c87aa3d6c921e18b50b6659311d5c2729b063ea0
Anuz Yadav <158555259+dark-cipher-26jan@users.noreply.github.com>
2024-04-02 07:40:49 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+function Strength(password) {
+  let i = 0;
+  if (password.length > 6) {
+    i++;
+  }
+  if (password.length >= 10) {
+    i++;
+  }
+
+  if (/[A-Z]/.test(password)) {
+    i++;
+  }
+
+  if (/[0-9]/.test(password)) {
+    i++;
+  }
+
+  if (/[A-Za-z0-8]/.test(password)) {
+    i++;
+  }
+
+  return i;
+}
+
+let container = document.querySelector(".container");
+document.addEventListener("keyup", function (e) {
+  let password = document.querySelector("#YourPassword").value;
+
+  let strength = Strength(password);
+  if (strength <= 2) {
+    container.classList.add("weak");
+    container.classList.remove("moderate");
+    container.classList.remove("strong");
+  } else if (strength >= 2 && strength <= 4) {
+    container.classList.remove("weak");
+    container.classList.add("moderate");
+    container.classList.remove("strong");
+  } else {
+    container.classList.remove("weak");
+    container.classList.remove("moderate");
+    container.classList.add("strong");
+  }
+});
+
+let password = document.querySelector("#YourPassword");
+let show = document.querySelector(".show");
+show.onclick = function () {
+  if (password.type === "password") {
+    password.setAttribute("type", "text");
+    show.classList.add("hide");
+  } else {
+    password.setAttribute("type", "password");
+    show.classList.remove("hide");
+  }
+};
+
+
+/*
+* Copyright (c) 2024 Anuz
+*
+* Author: Anuz Yadav
+* Email: darkcipher26jan2024@gmail.com
+* GitHub: https://github.com/dark-cipher-26jan
+*
+* This source code is licensed under the MIT license found in the
+* LICENSE file in the root directory of this source tree.
+*/
